3b5f80e5e4ff9c7a1b128125fc46e5bf25f445a8
root <zachary.chase@gmail.com>
2015-05-01 21:12:59 -0400

    adding l2 regularization (weight decay)

--- z_alpha_node_3c_2h.py
+++ z_alpha_node_3c_2h.py
@@ -30,35 +30,43 @@ def dropout(X, p=0.):
         X /= retain_prob
     return X
 
-def RMSprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
+def RMSprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6, lambda_2=.005):
     grads = T.grad(cost=cost, wrt=params)
+
     updates = []
     for p, g in zip(params, grads):
+      
+        ###########################
+        #  Adding weight decay
+        ###########################
+        g += lambda_2 * p
+        
+        
         acc = theano.shared(p.get_value() * 0.)
         acc_new = rho * acc + (1 - rho) * g ** 2
         gradient_scaling = T.sqrt(acc_new + epsilon)
         g = g / gradient_scaling
         updates.append((acc, acc_new))
-        updates.append((p, p - lr * g))
+        updates.append((p, p - lr * g ))
     return updates
 
 def model(X, w, w2, w3, w4, w5, w_o, b_h1, b_h2, b_o, p_drop_conv, p_drop_hidden):
   
-    l1_lin = conv2d(X, w, border_mode='full')+b_c1.dimshuffle('x', 0, 'x', 'x')
-    l1a = alpha_c1 * rectify(l1_lin) + (1.-alpha_c1) * T.tanh(l1_lin)
-    l1 = max_pool_2d(l1a, (2, 2))
-    l1 = dropout(l1, p_drop_conv)
+    l1_lin  = conv2d(X, w, border_mode='full')+b_c1.dimshuffle('x', 0, 'x', 'x')
+    l1a     = alpha_c1 * rectify(l1_lin) + (1.- alpha_c1) * T.tanh(l1_lin)
+    l1      = max_pool_2d(l1a, (2, 2))
+    l1      = dropout(l1, p_drop_conv)
 
     l2_lin = conv2d(l1, w2) + b_c2.dimshuffle('x', 0, 'x', 'x')
-    l2a = alpha_c2 * rectify(l2_lin) + (1.-alpha_c2) * T.tanh(l2_lin)
-    l2 = max_pool_2d(l2a, (2, 2))
-    l2 = dropout(l2, p_drop_conv)
+    l2a    = alpha_c2 * rectify(l2_lin) + (1. - alpha_c2) * T.tanh(l2_lin)
+    l2     = max_pool_2d(l2a, (2, 2))
+    l2     = dropout(l2, p_drop_conv)
 
     l3_lin = conv2d(l2, w3) + b_c3.dimshuffle('x', 0, 'x', 'x')
-    l3a = alpha_c3 * rectify(l3_lin) + (1.-alpha_c3) * T.tanh(l3_lin)
-    l3b = max_pool_2d(l3a, (2, 2))
-    l3 = T.flatten(l3b, outdim=2)
-    l3 = dropout(l3, p_drop_conv)
+    l3a    = alpha_c3 * rectify(l3_lin) + ( 1 - alpha_c3) * T.tanh(l3_lin)
+    l3b    = max_pool_2d(l3a, (2, 2))
+    l3     = T.flatten(l3b, outdim=2)
+    l3     = dropout(l3, p_drop_conv)
 
     l4_lin = T.dot(l3, w4) + b_h1 
     l4 = alpha_h1 * rectify(l4_lin) + (1.-alpha_h1) * T.tanh(l4_lin)
@@ -108,22 +116,33 @@ b_o = theano.shared(floatX(np.zeros(10)))
 alpha_c1 = theano.shared(floatX(np.asarray([.5,])), broadcastable=[True] )
 alpha_c2 = theano.shared(floatX(np.asarray([.5,])), broadcastable=[True] )
 alpha_c3 = theano.shared(floatX(np.asarray([.5,])), broadcastable=[True] )
+
 alpha_h1 = theano.shared(floatX(np.zeros(625) + .5))
 alpha_h2= theano.shared(floatX(np.zeros(625) + .5))
 
-noise_l1, noise_l2, noise_l3, noise_l4, noise_l5, noise_py_x = model(X, w, w2, w3, w4, w5, w_o, b_h1, b_h2, b_o, 0.2, 0.5)
+noise_l1, noise_l2, noise_l3, noise_l4, noise_l5, noise_py_x = model(X, w, w2, w3, w4, w5, w_o, b_h1, b_h2, b_o, 0.5, 0.5)
 l1, l2, l3, l4, l5, py_x = model(X, w, w2, w3, w4, w5, w_o, b_h1, b_h2, b_o, 0., 0.)
 y_x = T.argmax(py_x, axis=1)
 
 
+
+
 cost = T.mean(T.nnet.categorical_crossentropy(noise_py_x, Y))
 params = [w, w2, w3, w4, w5, w_o,
-          b_c1, b_c2, b_c3, b_h1, b_h2, b_o, alpha_h1, alpha_h2,
+          b_c1, b_c2, b_c3, b_h1, b_h2, b_o,
         ]
 updates = RMSprop(cost, params, lr=0.001)
-updates.append([alpha_c1, alpha_c1 - .001 * T.grad(cost, alpha_c1)])
-updates.append([alpha_c2, alpha_c2 - .001 * T.grad(cost, alpha_c2)])
-updates.append([alpha_c3, alpha_c3 - .001 * T.grad(cost, alpha_c3)])
+
+lambda_2 = .005
+
+updates.append([alpha_c1, alpha_c1 - .001 * T.grad(cost, alpha_c1) - .001 * lambda_2 * alpha_c1])
+updates.append([alpha_c2, alpha_c2 - .001 * T.grad(cost, alpha_c2) - .001 * lambda_2 * alpha_c1])
+updates.append([alpha_c3, alpha_c3 - .001 * T.grad(cost, alpha_c3) - .001 * lambda_2 * alpha_c1 ])
+
+updates.append([alpha_h1, alpha_h1 - .001 * T.grad(cost, alpha_h1) - .001 * lambda_2 * alpha_h1])
+updates.append([alpha_h2, alpha_h2 - .001 * T.grad(cost, alpha_h2) - .001 * lambda_2 * alpha_h2])
+
+
 
 
 train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
